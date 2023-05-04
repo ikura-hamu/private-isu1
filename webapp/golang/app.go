4137803d15766a2890f62282ca61e10543e60a09
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2023-05-05 00:26:15 +0900

    imageFolderPath修正

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -34,7 +34,7 @@ const (
 	postsPerPage     = 20
 	ISO8601Format    = "2006-01-02T15:04:05-07:00"
 	UploadLimit      = 10 * 1024 * 1024 // 10mb
-	imagesFolderPath = "../public/"
+	imagesFolderPath = "../public/image/"
 )
 
 type User struct {
