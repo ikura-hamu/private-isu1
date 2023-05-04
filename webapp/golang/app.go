67e08b49f8f6b8dda513da81713d8bd2c076b753
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2023-05-04 15:23:34 +0900

    makePostsのN+1

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -64,7 +64,7 @@ type Comment struct {
 	UserID    int       `db:"user_id"`
 	Comment   string    `db:"comment"`
 	CreatedAt time.Time `db:"created_at"`
-	User      User
+	User      User `db:"user"`
 }
 
 func init() {
@@ -192,8 +192,8 @@ func makePosts(results []Post, csrfToken string, allComments bool) ([]Post, erro
 			return nil, err
 		}
 
-		query := "SELECT * FROM `comments` WHERE `post_id` = ? ORDER BY `created_at` DESC"
-		// query := "SELECT u.*, c.* FROM `comments` AS c JOIN `users` AS u ON c.user_id = u.id WHERE c.post_id = ? ORDER BY c.created_at DESC"
+		// query := "SELECT * FROM `comments` WHERE `post_id` = ? ORDER BY `created_at` DESC"
+		query := "SELECT `comments`.*, users.id AS `user.id`, users.account_name AS `user.account_name`, users.passhash AS `user.passhash`, users.authority AS `user.authority`, users.del_flg AS `user.del_flg`, users.created_at AS `user.created_at` FROM `comments` JOIN `users` ON comments.user_id = users.id WHERE comments.post_id = 10 ORDER BY comments.created_at DESC LIMIT 3"
 		if !allComments {
 			query += " LIMIT 3"
 		}
@@ -203,14 +203,12 @@ func makePosts(results []Post, csrfToken string, allComments bool) ([]Post, erro
 			return nil, err
 		}
 
-		// _  = "SELECT u.*, c.* FROM `comments` AS c JOIN `users` AS u ON c.user_id = u.id WHERE c.post_id = ?"
-
-		for i := 0; i < len(comments); i++ {
-			err := db.Get(&comments[i].User, "SELECT * FROM `users` WHERE `id` = ?", comments[i].UserID)
-			if err != nil {
-				return nil, err
-			}
-		}
+		// for i := 0; i < len(comments); i++ {
+		// 	err := db.Get(&comments[i].User, "SELECT * FROM `users` WHERE `id` = ?", comments[i].UserID)
+		// 	if err != nil {
+		// 		return nil, err
+		// 	}
+		// }
 
 		// reverse
 		for i, j := 0, len(comments)-1; i < j; i, j = i+1, j-1 {
